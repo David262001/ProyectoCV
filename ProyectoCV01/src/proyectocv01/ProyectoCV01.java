d43155921dd59eaef10d3c8eeb4966732a580426
David262001 <dteran3980@uta.edu.ec>
2022-06-12 22:49:25 -0500

    Creacion de sout

--- ProyectoCV01/src/proyectocv01/ProyectoCV01.java
+++ ProyectoCV01/src/proyectocv01/ProyectoCV01.java
@@ -15,7 +15,7 @@ public class ProyectoCV01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("David Teran");
     }
     
 }
